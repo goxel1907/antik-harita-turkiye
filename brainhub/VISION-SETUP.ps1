9b883e7cf54c149033e9a37386441f7f8b4c3c17
goxel1907 <127673011+goxel1907@users.noreply.github.com>
2026-09-18 19:13:46 +0300

    Keep legacy Vision setup wrapper bootstrap-compatible

---
+++ brainhub/VISION-SETUP.ps1
@@ -0,0 +1,2 @@
+Write-Host 'VISION-SETUP artik anahtar istemez; BrainHub 9Router icindeki mevcut OpenRouter provider ayarini kullanir.' -ForegroundColor Yellow
+& "$PSScriptRoot\manage.ps1" -Action VisionStatus @args
